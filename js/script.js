69b2bf3b81226c0f1e43b218b1547c949e5e9115
yNickFtw <nkxynickftw@gmail.com>
2022-10-22 21:34:23 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function calcular(n1, n2) {
+    n1 = Number(document.getElementById("n1").value)
+    n2 = Number(document.getElementById("n2").value)
+
+    selector = document.getElementById("selector").value
+
+    switch (selector) {
+        case '+':
+            calculo = (n1 + n2)
+            document.getElementById("resultado").innerHTML = `O resultado da soma de ${n1} + ${n2} é = ${calculo}`
+
+            break
+
+        case '-':
+            calculo = (n1 - n2)
+            document.getElementById(document.getElementById("resultado").innerHTML = `O resultado da subtração de ${n1} - ${n2} é = ${calculo}`)
+
+            break
+
+        case '/':
+            calculo = (n1 / n2)
+            document.getElementById(document.getElementById("resultado").innerHTML = `O resultado da divisão de ${n1} / ${n2} é = ${calculo}`)
+
+            break
+
+        case '*':
+            calculo = (n1 * n2)
+            document.getElementById(document.getElementById("resultado").innerHTML = `O resultado da multiplicação de ${n1} x ${n2} é = ${calculo}`)
+
+    }
+}
